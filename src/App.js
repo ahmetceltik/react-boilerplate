c5b842ab5ad5c35293f85ab2402c426cb7b1f9ad
Ahmet Celtik <ahmetceltik77@gmail.com>
2022-09-21 22:34:21 +0100

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 
+import Button from '@mui/material/Button';
+
 export default function App() {
 
     const [counter, setCounter] = useState(0);
@@ -9,9 +11,9 @@ export default function App() {
         <div style={{display: 'flex', flexDirection:'column', alignItems: 'center', textAlign: 'center', justifyContent: 'space-evenly'}}>
             <h2>I want to play a game</h2>
             <h4>You clicked me {counter} times</h4>
-            <button onClick={() => {
+            <Button variant='outlined' onClick={() => {
                 setCounter(counter + 1)
-            }}> I am clickable! </button>
+            }}> I am clickable! </Button>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, sunt!</p>
             <h1 style={{color: 'pink', fontSize: '80px'}}> SCOPERSKI IS A BITCH </h1>
         </div>
